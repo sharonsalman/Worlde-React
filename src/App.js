1e6c96783b21f1f4adad87d5c9dd363faab89841
sharonsalman <sharon.salman1@gmail.com>
2022-07-27 10:49:19 +0300

    Making the board

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Keyboard from './components/Keyboard';
+import Board from './components/Board';
+import { createContext } from "react";
+
+
+export const Appcontext = createContext();
 
 function App() {
+  const [board , setBoard] = useState(boardDefaults);
   return (
     <div className="App">
-     
+     <nav>
+      <h1>Worlde</h1>
+     </nav>
+     <Appcontext.Provider value= {{board, setBoard}}>
+     <Board />
+     <Keyboard />
+    </Appcontext.Provider>
     </div>
   );
 }
---
+++ src/components/Letter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+function Letter(letterPosition, attemptValue) {
+    const letter = board[attemptValue][letterPosition];
+  return (
+    <div className= "Letter">
+        Letter
+        </div>
+  )
+}
+
+export default Letter
